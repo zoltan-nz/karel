3010193da7ee6afb35e48596f1565a7a5a6b9365
Zoltan <zdebre@gmail.com>
2014-08-31 22:21:54 +1200

    Target solved

--- Assignment2/src/main/java/Target.java
+++ Assignment2/src/main/java/Target.java
@@ -12,6 +12,18 @@ import java.awt.*;
 
 public class Target extends GraphicsProgram {	
 	public void run() {
-		/* You fill this in. */
-	}
+	    int middleX = getWidth()/2;
+        int middleY = getHeight()/2;
+
+        for(int i=3; i>0; i--) {
+            add(filledCircle(middleX, middleY, (int)(72/((double)3/(double)i)), (i%2 == 0) ? Color.WHITE : Color.RED));
+        }
+    }
+
+    private static GOval filledCircle(int x, int y, int r, Color c) {
+        GOval o = new GOval(x-r, y-r, 2*r, 2*r);
+        o.setFilled(true);
+        o.setColor(c);
+        return o;
+    }
 }
