7185abe4b9638c50c67542e20584b7f21a65fb4b
Zoltan <zdebre@gmail.com>
2014-09-05 21:57:13 +1200

    Dice roller

--- Assignment2/src/main/java/SimpleRandom.java
+++ Assignment2/src/main/java/SimpleRandom.java
@@ -3,9 +3,32 @@ import acm.util.*;
 
 public class SimpleRandom extends ConsoleProgram {
 
+    private static final int KOCKA_OLDAL_SZAMA = 6;
+
     public void run() {
-        int kockaDobas = myRGen.nextInt(1, 6);
-        println("Dobott szam: " + kockaDobas);
+
+        while(true) {
+            int kockakSzama = readInt("Hany darab kockad van? ");
+            int maxEzJohetKi = kockakSzama * KOCKA_OLDAL_SZAMA;
+            int dobasSzamlalo = 0;
+            while (true) {
+                int dobottOsszeg = rollDice(kockakSzama);
+                dobasSzamlalo++;
+                if (dobottOsszeg == maxEzJohetKi) break;
+                println(dobottOsszeg);
+            }
+
+            println(dobasSzamlalo + " " + maxEzJohetKi);
+            if (readInt("Exit for 0: ") == 0) break;
+        }
+    }
+
+    private int rollDice(int numDice) {
+        int ossszesen = 0;
+        for (int i=0; i<numDice; i++) {
+            ossszesen += myRGen.nextInt(1, KOCKA_OLDAL_SZAMA);
+        }
+        return ossszesen;
     }
 
     private RandomGenerator myRGen = RandomGenerator.getInstance();
